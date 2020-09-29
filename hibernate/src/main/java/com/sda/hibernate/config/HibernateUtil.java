839edee2e727108f6aedd3fe9c1b535d82a05a64
cosmin_bucur <cosmindanbucur@gmail.com>
2020-09-29 22:06:24 +0300

    add one to many bidirectional

--- hibernate/src/main/java/com/sda/hibernate/config/HibernateUtil.java
+++ hibernate/src/main/java/com/sda/hibernate/config/HibernateUtil.java
@@ -1,5 +1,7 @@
 package com.sda.hibernate.config;
 
+import com.sda.hibernate.associations.one_to_many_bi.Child;
+import com.sda.hibernate.associations.one_to_many_bi.Parent;
 import com.sda.hibernate.associations.one_to_many_uni.Daughter;
 import com.sda.hibernate.associations.one_to_many_uni.Mother;
 import com.sda.hibernate.associations.one_to_many_uni_join.Father;
@@ -32,7 +34,7 @@ public class HibernateUtil {
         settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
         settings.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate?serverTimezone=UTC");
         settings.put(Environment.USER, "root");
-        settings.put(Environment.PASS, "Rootpass3#");
+        settings.put(Environment.PASS, "root");
         settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
         settings.put(Environment.SHOW_SQL, "true");
         settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
@@ -46,6 +48,8 @@ public class HibernateUtil {
         configuration.addAnnotatedClass(Daughter.class);
         configuration.addAnnotatedClass(Father.class);
         configuration.addAnnotatedClass(Son.class);
+        configuration.addAnnotatedClass(Child.class);
+        configuration.addAnnotatedClass(Parent.class);
 
         return configuration;
     }
